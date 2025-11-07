73c4da5c9c6388f0f990f9337eb006eedfedd97a
bargida <bargida.tilyakova25@gmail.com>
2025-11-07 15:58:45 +0500

    task is done

--- arithmetic_op05.py
+++ arithmetic_op05.py
@@ -1,5 +1,5 @@
 # Create a variable "a"  and assign it the value of the int 72.
-
+a = 72
 # Create a variable "b"  and assign it the value of the int 9.
-
+b 
 # Create a variable called "answer" and assign it the division of a by b.
